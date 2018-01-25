81672387eaaca7bade5bf69917e54083543f911e
Kevin Harrington <mad.hephaestus@gmail.com>
2018-01-25 10:03:35 -0500

    adding comments

--- hotfix.groovy
+++ hotfix.groovy
@@ -1,5 +1,5 @@
 println "Launching https://github.com/CommonWealthRobotics/HotfixBowlerStudio/new/master"
-
+//https://github.com/madhephaestus/BowlerSlicer.git
 ScriptingEngine.gitScriptRun(
 	                "https://gist.github.com/44d3d390483b3d1bec11b63ac0c012a6.git", // git location of the library
 	                      "SliceTest.groovy" , // file to load
