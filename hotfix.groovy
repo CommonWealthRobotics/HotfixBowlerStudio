5ea06e89f5272ab6ac2b8f7916703e58c4f99ea1
Kevin Harrington <mad.hephaestus@gmail.com>
2018-01-25 09:38:37 -0500

    moving slicer to hotfix slicer repository

--- hotfix.groovy
+++ hotfix.groovy
@@ -1,7 +1,7 @@
 println "Launching https://github.com/CommonWealthRobotics/HotfixBowlerStudio/new/master"
 
 ScriptingEngine.gitScriptRun(
-	                "https://gist.github.com/44d3d390483b3d1bec11b63ac0c012a6.git", // git location of the library
+	                "https://github.com/madhephaestus/BowlerSlicer.git", // git location of the library
 	                      "SliceTest.groovy" , // file to load
 	                      []// no parameters but not null
                         );
