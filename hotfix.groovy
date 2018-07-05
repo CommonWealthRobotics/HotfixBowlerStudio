4c0690d84837f97eace7a06ba032815e197e6e81
Kevin Harrington <mad.hephaestus@gmail.com>
2018-07-05 13:51:02 -0400

    load slicer only on older builds

--- hotfix.groovy
+++ hotfix.groovy
@@ -1,7 +1,12 @@
 println "Launching https://github.com/CommonWealthRobotics/HotfixBowlerStudio/new/master"
+int[] version = com.neuronrobotics.bowlerstudio.assets.StudioBuildInfo.getBuildInfo();
 //https://github.com/madhephaestus/BowlerSlicer.git
+if(version[0]<=0 && version[1]<=27&& version[2]<=1){
 ScriptingEngine.gitScriptRun(
 	                "https://github.com/madhephaestus/CSGSlicer.git", // git location of the library
 	                      "slice.groovy" , // file to load
 	                      []// no parameters but not null
                         );
+}else{
+	println "Using internal slicer"
+}
