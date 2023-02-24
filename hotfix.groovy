5a6dcca39455951b77bd34c8bd79fc6bf5b9df0c
Kevin Harrington <mad.hephaestus@gmail.com>
2023-02-24 14:14:20 -0500

    remove old slicing

--- hotfix.groovy
+++ hotfix.groovy
@@ -1,15 +1,7 @@
 println "Launching https://github.com/CommonWealthRobotics/HotfixBowlerStudio/new/master"
 int[] version = com.neuronrobotics.bowlerstudio.assets.StudioBuildInfo.getBuildInfo();
 //https://github.com/madhephaestus/BowlerSlicer.git
-if(version[0]<=0 && version[1]<=27&& version[2]<=1){
-ScriptingEngine.gitScriptRun(
-	                "https://github.com/madhephaestus/CSGSlicer.git", // git location of the library
-	                      "slice.groovy" , // file to load
-	                      []// no parameters but not null
-                        );
-}else{
-	println "Using internal slicer"
-}
+
 if(version[0]<=1 && version[1]<=3&& version[2]<=1)
 ScriptingEngine.gitScriptRun("https://github.com/CommonWealthRobotics/DeviceProviders.git",
 											"loadAll.groovy",[])
